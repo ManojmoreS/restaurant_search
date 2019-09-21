a538282982c6455150c854091ee90c124722acbd
Manoj More S <lettertomanojmore@gmail.com>
2019-09-21 17:10:15 +0530

    Phase 2 - Refactored the code to make api call and handle result load

--- src/App.js
+++ src/App.js
@@ -1,118 +1,50 @@
 import React, { Component } from 'react';
 import { Container, Row, Col} from 'react-bootstrap'
-import { compose, withProps, lifecycle, withHandlers } from "recompose"
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/App.css';
 import Result from './Result';
-const { StandaloneSearchBox } = require("react-google-maps/lib/components/places/StandaloneSearchBox");
-
-const PlacesWithStandaloneSearchBox = compose(
-  withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDAQOhuvUriLPgDzVblnSSH7BUj-s2EMSw&v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
-  }),
-  lifecycle({
-    componentDidMount() {
-      const refs = {}
-
-      this.setState({
-        places: [],
-        lat: 40.7127753,
-        lng: -74.0059728,
-        onSearchBoxMounted: ref => {
-          refs.searchBox = ref;
-        },
-        onPlacesChanged: () => {
-          const places = refs.searchBox.getPlaces();
-          places.map(
-            ({ place_id, formatted_address, geometry: { location } }) =>
-            this.setState({
-              places,
-              lat: location.lat(),
-              lng: location.lng()
-            })
-          )
-        },
-      })
-    },
-  }),
-  withScriptjs
-)(props =>
-  <Container>
-    <Row>
-    <Col md={4}>
-      <Result data={{term: 'food', lat: props.lat, lng: props.lng}}/>
-    </Col>
-    <Col md={{ span: 8}}>
-    <div data-standalone-searchbox="">
-      <StandaloneSearchBox
-        ref={props.onSearchBoxMounted}
-        bounds={props.bounds}
-        onPlacesChanged={props.onPlacesChanged}
-      >
-        <input
-          className='search_box'
-          type="text"
-          placeholder="Customized your placeholder"
-          style={{
-            boxSizing: `border-box`,
-            borderRadius: `3px`,
-            boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-            fontSize: `14px`,
-            textOverflow: `ellipses`,
-          }}
-        />
-      </StandaloneSearchBox>
-      {props.places.map(({ place_id, formatted_address, geometry: { location } }) =>
-      <MapWithAMarker
-      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDAQOhuvUriLPgDzVblnSSH7BUj-s2EMSw&v=3.exp&libraries=geometry,drawing,places"
-      loadingElement={<div style={{ height: `100%` }} />}
-      containerElement={<div style={{ height: `400px` }} />}
-      mapElement={<div style={{ height: `100%` }} />}
-      lat_lng={ {lat:props.lat, lng:props.lng}}
-      />
-      )}
-
-      <ol>
-        {props.places.map(({ place_id, formatted_address, geometry: { location } }) =>
-          <ul key={place_id}>
-            {formatted_address}
-            {" at "}
-            ({location.lat()}, {location.lng()})
-          </ul>
-        )}
-      </ol>
-    </div>
-    </Col>
-    </Row>
-  </Container>
-);
-const MapWithAMarker = withScriptjs(withGoogleMap(props =>
-  <GoogleMap defaultZoom={8} defaultCenter={{ lat: props.lat_lng.lat, lng: props.lat_lng.lng }}>
-    <Marker position={{ lat: props.lat_lng.lat, lng: props.lat_lng.lng }} />
-  </GoogleMap>
-));
+import MoreMap from './map';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      term: 'Food',
-      lat: 40.7128,
-      lng: 74.0060
-    };
+    this.handlerMapSeacrh = this.handlerMapSeacrh.bind(this);
+    this.state = { lat: 0, lng: 0 };
+  }
+
+  handlerMapSeacrh(childdata) {
+    var geoloc = childdata.markers[0]
+    if(geoloc){
+      this.setState({ lat: geoloc.position.lat(), lng: geoloc.position.lng()})
+    }
+    console.log('Got map state');
+    console.log(this.state);
   }
 
   render() {
     return (
       <div className="App">
-        <PlacesWithStandaloneSearchBox />
+      <Container>
+        <Row>
+          <Col md={4}>
+          Yelp Result
+          </Col>
+          <Col md={{ span: 8}}>
+          GoogleMap
+          </Col>
+        </Row>
+        <Row>
+          <Col md={4}>
+            <Result data={this.state}/>
+          </Col>
+          <Col md={{ span: 8}}>
+          <MoreMap handlerMapSeacrh={this.handlerMapSeacrh}/>
+          </Col>
+        </Row>
+      </Container>
       </div>
     );
   }
 }
-
 export default App;
